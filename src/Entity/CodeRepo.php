8ee5e41d2bdd241fb31984fec560df7241085e6a
Jakub Zienkiewicz <jakub@zienkiewi.cz>
2022-03-18 13:24:58 +0100

    Create basic repository/organization entities and migration

---
+++ src/Entity/CodeRepo.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CodeRepoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CodeRepoRepository::class)]
+class CodeRepo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\ManyToOne(targetEntity: Organization::class, inversedBy: 'codeRepos')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $organization;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $creation_date;
+
+    #[ORM\Column(type: 'float')]
+    private $trust_score;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOrganization(): ?Organization
+    {
+        return $this->organization;
+    }
+
+    public function setOrganization(?Organization $organization): self
+    {
+        $this->organization = $organization;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(?\DateTimeInterface $creation_date): self
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+
+    public function getTrustScore(): ?float
+    {
+        return $this->trust_score;
+    }
+
+    public function setTrustScore(float $trust_score): self
+    {
+        $this->trust_score = $trust_score;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Organization.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrganizationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrganizationRepository::class)]
+class Organization
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\OneToMany(mappedBy: 'organization', targetEntity: CodeRepo::class, orphanRemoval: true)]
+    private $codeRepos;
+
+    public function __construct()
+    {
+        $this->codeRepos = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, CodeRepo>
+     */
+    public function getCodeRepos(): Collection
+    {
+        return $this->codeRepos;
+    }
+
+    public function addCodeRepo(CodeRepo $codeRepo): self
+    {
+        if (!$this->codeRepos->contains($codeRepo)) {
+            $this->codeRepos[] = $codeRepo;
+            $codeRepo->setOrganization($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCodeRepo(CodeRepo $codeRepo): self
+    {
+        if ($this->codeRepos->removeElement($codeRepo)) {
+            // set the owning side to null (unless already changed)
+            if ($codeRepo->getOrganization() === $this) {
+                $codeRepo->setOrganization(null);
+            }
+        }
+
+        return $this;
+    }
+}
