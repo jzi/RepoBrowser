f1eea0c8deae21ef46513423dc015cd3e4adeb5c
Jakub Zienkiewicz <jakub@zienkiewi.cz>
2022-03-21 14:20:19 +0100

    Enable fetching Github repositories

--- src/Provider/GithubProvider.php
+++ src/Provider/GithubProvider.php
@@ -3,10 +3,46 @@
 namespace App\Provider;
 
 use App\Provider\Provider;
+use GuzzleHttp\Client;
 
 class GithubProvider extends Provider {
 
-    public function import(string $organization):bool {
-        return false;
+    const BASE_URL = 'https://api.github.com';
+
+    private $client;
+
+    public function __construct() {
+        $this->client = new Client(['base_uri' => self::BASE_URL]);
     }
+
+    public function import(string $organization):bool {
+        $url = $this->getReposListURL($organization);
+        $response = $this->client->get($url);
+        $status = $response->getStatusCode();
+
+        if ($status == 200) {
+            $body = $response->getBody();
+            $json = json_decode($body, true);
+            foreach ($json as $item) {
+                $name = $item['name'];
+                $url = $item['url'];
+                $response = $this->client->get($url);
+                $status = $response->getStatusCode();
+                $body = $response->getBody();
+                $repoJson = json_decode($body, true);
+
+                var_dump($repoJson);
+
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private function getReposListUrl(string $organization):string {
+        return "orgs/{$organization}/repos?per_page=3";
+    }
+
+
 }
