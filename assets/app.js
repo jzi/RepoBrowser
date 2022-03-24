c596be86744630844a3d1679a229cf317be51a51
Jakub Zienkiewicz <jakub@zienkiewi.cz>
2022-03-24 11:36:02 +0100

    Enable sorting the repositories list by different fields

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,13 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/global.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import $ from 'jquery';
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+require('bootstrap-table');
