40ab440bd357058164a0fce54dc77ec96f208035
Jakub Zienkiewicz <jakub.zienkiewicz@iteo.com>
2022-06-08 10:57:37 +0200

    Prepare environment for running Behat tests

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 composer install
+
+bin/console doctrine:database:create --if-not-exists
+bin/console doctrine:migrations:migrate
+
+bin/console doctrine:database:create --if-not-exists --env=test
+bin/console doctrine:migrations:migrate --env=test
+
 npm install
 npm run build
-bin/console doctrine:migrations:migrate
-bin/console doctrine:migrations:migrate --env=test
 
 exec "$@"
