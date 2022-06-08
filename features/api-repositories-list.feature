4644e57e8ce2249ab67ed1ee90e9a662327a844e
Jakub Zienkiewicz <jakub.zienkiewicz@iteo.com>
2022-06-08 10:57:55 +0200

    Test API repositories list

---
+++ features/api-repositories-list.feature
@@ -0,0 +1,15 @@
+Feature: Listing repositories
+  Background:
+    Given the following repositories exist
+      | name       | organization_name | creation_date | trust_score |
+      | test-repo1 | test-org1         | 1970-01-01    |  0          |
+      | test-repo2 | test-org1         | 1980-01-01    |  5          |
+      | test-repo3 | test-org2         | 1990-01-01    | 10          |
+  Scenario: I want to list known repositories
+    When I request "/api/repositories.json" using HTTP "GET"
+    Then the response code is "200"
+    And  the response body is a JSON array with a length of at least "3"
+    And  the response body is a JSON array with a length of at most "3"
+    And  the response contains the repository "test-repo1" from organization "test-org1"
+    And  the response contains the repository "test-repo2" from organization "test-org1"
+    And  the response contains the repository "test-repo3" from organization "test-org2"
---
+++ tests/Behat/FeatureContext.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Tests\Behat;
+
+use App\Entity\CodeRepo;
+use App\Entity\Organization;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\TableNode;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use Imbo\BehatApiExtension\Context\ApiContext;
+use Imbo\BehatApiExtension\Exception\AssertionFailedException;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends ApiContext
+{
+    private EntityManagerInterface $entityManager;
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    /**
+     * @Given the following repositories exist
+     */
+    public function theFollowingRepositoriesExist(TableNode $table)
+    {
+
+        foreach ($table as $item)
+        {
+            $organization = new Organization();
+            $organization->setName($item['organization_name']);
+            $codeRepo = new CodeRepo();
+            $codeRepo->setName($item['name'])
+                ->setCreationDate(new \DateTime($item['creation_date']))
+                ->setTrustScore($item['trust_score'])
+                ->setOrganization($organization);
+            $this->entityManager->persist($codeRepo);
+        }
+
+        $this->entityManager->flush();
+    }
+
+    /**
+     * @Then the response contains the repository :repository from organization :organization
+     * @throws AssertionFailedException
+     */
+    public function theResponseContainsTheRepositoryFromOrganization($repository, $organization)
+    {
+        $json = json_decode($this->response->getBody(), true);
+
+        foreach ($json as $item)
+        {
+            if (
+                array_key_exists('name', $item) &&
+                $item['name'] == $repository &&
+                array_key_exists('organization', $item) &&
+                array_key_exists('name', $item['organization']) &&
+                $item['organization']['name'] == $organization
+            )
+                return true;
+        }
+
+        throw new AssertionFailedException("Repository {$repository} of organization {$organization} not found in response");
+    }
+
+    /**
+     * @BeforeSuite
+     *
+     * @return void
+     */
+    public static function beforeSuite()
+    {
+        return self::prepareDatabase();
+    }
+
+    private function purgeDatabase()
+    {
+        $purger = new ORMPurger($this->entityManager);
+        $purger->purge();
+    }
+
+    private static function prepareDatabase()
+    {
+        $commands = [
+            "bin/console doctrine:database:drop -n --if-exists --env=test --force",
+            "bin/console doctrine:database:create -n --if-not-exists --env=test",
+            "bin/console doctrine:migrations:migrate -n --env=test"
+        ];
+
+        foreach ($commands as $cmd) exec ($cmd);
+    }
+
+}
